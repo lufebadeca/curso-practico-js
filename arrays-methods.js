46026ea855d4df26f33b7381b14fb01d7edc630c
lufebadeca <lufebadeca@gmail.com>
2022-06-06 22:53:17 -0500

    array file for reference updated

--- arrays-methods.js
+++ arrays-methods.js
@@ -5,4 +5,29 @@ var articulos = [
     {nombre: "automovil", costo: 5000}
 ]
 
-var bicicleta = articulos.find(function(artic){return articulos.nombre === "bicicleta"});
+//IMPORTANT: starting point: an objects array, hence each element has several properties
+
+var articulosFiltrados = articulos.filter(function(artic){
+    return articulos.costo >= 500;
+    //returns a new array of all the elements of the original array that comply with the specified criteria
+});
+
+var articulosmapeados = articulos.map(function(artic){
+    return articulos.nombre;
+    //returns a new array with the desired property for all elements
+});
+
+var hayBicicleta = articulos.find(function(artic){
+    return articulos.nombre === "bicicleta";
+    //returns an object from the object array that complies with the search criteria
+});
+
+var articulosbaratos = articulos.some(function(articulo){
+    return articulo.costo <=700;
+    //returns a boolean if any element of the array complies with the condition
+});
+
+articulos.forEach(function(articulo){
+    console.log(articulo.nombre);
+    //returns the specified info for each elements of the array
+});
