ba8b09f66503c31b05e0cb8623d0544445bafed9
lufebadeca <lufebadeca@gmail.com>
2022-06-09 23:36:09 -0500

    graphic interface added with canvas

--- figuras.js
+++ figuras.js
@@ -1,87 +1,110 @@
-console.group("cuadrado");
+var area1 = document.getElementById("canvas1");
+var lienzo1 =area1.getContext("2d");
+console.group("lienzos");
+var area2 = document.getElementById("canvas2");
+var lienzo2 =area2.getContext("2d");
+var area3 = document.getElementById("canvas3");
+var lienzo3 =area3.getContext("2d");
+console.log(lienzo1, lienzo2, lienzo3);
+console.groupEnd();
 
 const ladoCuadrado = 5;
-console.log("Lado del cuadrado mide: " + ladoCuadrado + "cm");
 
 function perimetroCuadrado(lado){
     return lado*4;
 }
 
-console.log("Perimetro del cuadrado mide: " + perimetroCuadrado(ladoCuadrado) + "cm");
-
 function areaCuadrado(lado){
     return lado*lado;
 }
-
-console.log("Area del cuadrado es: " + areaCuadrado(ladoCuadrado) + "cm2");
-
-console.groupEnd();
-
-console.group("Triangulo");
-const ladoTriangulo1 = 3;
-const ladoTriangulo2 = 3;
-const baseTriangulo = 2;
-const alturaTriangulo = 2.5;
-console.log("Lados del Triangulo: " + ladoTriangulo1 + " y " + ladoTriangulo2 + "cm");
 
 function perimetroTriangulo(lado1,lado2,base){
     return lado1+lado2+base;
 }
 
-console.log("Perimetro del Triangulo es: " + perimetroTriangulo(ladoTriangulo1,ladoTriangulo2,baseTriangulo) + "cm");
-
 function areaTriangulo(base, altura){
     return (base*altura)/2;
 }
 
-console.log("Area del Triangulo es: " + areaTriangulo(baseTriangulo,alturaTriangulo) + "cm2");
-console.groupEnd();
-
-console.group("Circulo");
 const radio = 3;
 const diametro = 2*radio;
 const pi = Math.PI;
-
-console.log("Radio del Circulo: " + radio + "cm");
 
 function perimetroCirculo(diam){
     return pi*diam;
 }
 
-console.log("Perimetro del Circulo es: " + perimetroCirculo(diametro) + "cm");
-
 function areaCirculo(rad){
     return (pi*rad*rad);
 }
 
-console.log("Area del Circulo es: " + areaCirculo(radio) + "cm2");
-console.groupEnd();
+function drawLine(lienzo, color, xi, yi, xf, yf)  //PASOS ORIGINALES PARA CREAR UNA LÍNEA
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle= color;
+  lienzo.moveTo(xi, yi);
+  lienzo.lineTo(xf, yf);
+  lienzo.stroke();
+  lienzo.closePath();
+}
+function drawSquare(){
+    ancho = area1.width;
+    alto = area1.height;
+    drawLine(lienzo1, "blue", ancho/6, alto/6, ancho*5/6, alto/6);
+    drawLine(lienzo1, "blue", ancho*5/6, alto/6, ancho*5/6, alto*5/6);
+    drawLine(lienzo1, "blue", ancho*5/6, alto*5/6, ancho/6, alto*5/6);
+    drawLine(lienzo1, "blue", ancho/6, alto*5/6, ancho/6, alto/6);
+}
+function drawTriangle(height, base){
+    var ancho = Number(area3.width);
+    var alto = Number(area3.height);
+    alert(ancho + " " + base +" "+ alto +" " + height);
+    drawLine(lienzo3, "green", ancho/2, (alto-height)/2, (ancho+Number(base))/2, (alto+Number(height))/2);
+    drawLine(lienzo3, "red", (ancho+Number(base))/2, (alto+Number(height))/2, (ancho-base)/2, (alto+Number(height))/2);
+    drawLine(lienzo3, "blue", (ancho-base)/2, (alto+height)/2, ancho/2, (alto-height)/2);
+}
+
+function drawCircle(color){
+    ancho = area2.width;
+    alto = area2.height;
+    lienzo2.beginPath();
+    lienzo2.strokeStyle = color;
+    lienzo2.arc(ancho/2,alto/2,ancho/3,0,2*Math.PI);
+    lienzo2.stroke();
+    lienzo2.closePath();
+}
 
 function computeP(){
     const inputSquare = document.getElementById("square-input");
     const inputValue = inputSquare.value;
     var perim = perimetroCuadrado(inputValue);
-    alert(perim + "cm");
+    const squarePerimField = document.getElementById("square-perim-result");
+    squarePerimField.innerText = `Perimeter = ${perim} cm`;
 }
 
 function computeA(){
     const inputSquare = document.getElementById("square-input");
     const inputValue = inputSquare.value;
     var area = areaCuadrado(inputValue);
-    alert(area + "cm2");
+    const squareAreaField = document.getElementById("square-area-result");
+    squareAreaField.innerText = `Area = ${area} cm2`;
+    drawSquare();
 }
 function computeCP(){
     const inputRad = document.getElementById("circle-input");
     const radValue = inputRad.value;
     var perim = perimetroCirculo(radValue*2);
-    alert(perim + "cm");
+    const circlePerimField = document.getElementById("circle-perim-result");
+    circlePerimField.innerText = `Perim = ${perim} cm`;
 }
 
 function computeCA(){
     const inputRad = document.getElementById("circle-input");
     const radValue = inputRad.value;
     var area = areaCirculo(radValue);
-    alert(area + "cm2");
+    const circleAreaField = document.getElementById("circle-area-result");
+    circleAreaField.innerText = `Area = ${area} cm2`;
+    drawCircle("red");
 }
 function computeTP(){
     const inputTrian1 = document.getElementById("trian1-input");
@@ -91,7 +114,8 @@ function computeTP(){
     const inputTrian3 = document.getElementById("trianB-input");
     const base = Number(inputTrian3.value);
     var perim = perimetroTriangulo(lado1, lado2, base);
-    alert(perim + "cm");
+    const triangPerimField = document.getElementById("triangle-perim-result"); 
+    triangPerimField.innerText = perim + " cm";
 }
 
 function computeTA(){
@@ -101,13 +125,22 @@ function computeTA(){
     const lado2 = inputTrian2.value;
     const inputTrian3 = document.getElementById("trianB-input");
     const base = inputTrian3.value;
-    if (lado1 == lado2){
-        const hipo = Math.sqrt(Math.pow(lado1,2)-(Math.pow(base,2)/4));
-        var area = areaTriangulo(base, hipo);
-        alert("Altura: "+ hipo + ". Area: " + area + "cm2");
+    alert(lado1 + ", " + lado2 + ", "+ base);
+    if (lado1==0 || lado2==0 || base==0){
+        alert("Error, all triangle sides must be indicated");
     }
     else{
-        alert("Error, unable to compute height since the length of side 1 and 2 are different")
+        if (lado1 == lado2){
+            var hipo = Math.sqrt(Math.pow(lado1,2)-(Math.pow(base,2)/4));
+            const heightField = document.getElementById("height-box");
+            heightField.innerText = hipo;
+            var area = areaTriangulo(base, hipo);
+            const triangAreaField = document.getElementById("triangle-area-result");
+            triangAreaField.innerText = area + " cm2";
+            drawTriangle(hipo, base);
+        }
+        else{
+            alert("Error, unable to compute height since the length of side 1 and 2 are different");
+        }
     }
-
 }
